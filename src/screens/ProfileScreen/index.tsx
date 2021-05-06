0ffd79edab6b12126758c150ce0297ba763303ef
Tomek Felisiak <tomekfel@googlemail.com>
2021-05-06 10:03:12 +0100

    Profile screen UI

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import styles from "./styles";
 
+const image = require("../../../assets/images/Saly-16.png");
+
 const ProfileScreen = () => {
+  const [user, setUser] = React.useState({
+    id: "1",
+    name: "Prince Charming",
+    email: "email@email.com",
+    image: "https://assets.coingecko.com/coins/images/1/small/bitcoin.png",
+    netWorth: 100000,
+  });
+
+  const signOut = () => {
+    console.warn("Sign out");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <Image style={styles.image} source={image} />
+      <View style={styles.userContainer}>
+        <View style={styles.left}>
+          <Image style={styles.userImage} source={{ uri: user.image }} />
+          <View>
+            <Text style={styles.name}>{user.name}</Text>
+            <Text style={styles.email}>{user.email}</Text>
+          </View>
+        </View>
+
+        <View style={styles.right}>
+          <Text style={styles.netWorth}>${user.netWorth}</Text>
+        </View>
+      </View>
+      <Pressable onPress={signOut} style={styles.signout}>
+        <Text>Sign out</Text>
+      </Pressable>
     </View>
   );
 };
--- src/screens/ProfileScreen/styles.ts
+++ src/screens/ProfileScreen/styles.ts
@@ -3,8 +3,44 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+    backgroundColor: "white",
+  },
+  image: {
+    height: 175,
+    resizeMode: "contain",
+  },
+  userImage: {
+    height: 100,
+    width: 100,
+    marginRight: 10,
+    borderRadius: 50,
+  },
+  userContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 50,
+    marginVertical: 10,
+    width: "100%",
+  },
+  left: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  right: {
+    alignItems: "flex-end",
+  },
+  name: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  email: {},
+  netWorth: {},
+
+  signout: {
+    marginTop: "auto",
   },
 });
 
